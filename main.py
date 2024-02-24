83173af4eeffa832c8ed8af351741a8d2dee79c7
kpsofteng <kevin.palmer@gmail.com>
2024-02-24 15:04:08 -0500

    move functionality to Book class

--- main.py
+++ main.py
@@ -1,27 +1,41 @@
-def get_book_text(book_path):
-    with open(book_path) as f:
-        return f.read()
-    
-def get_word_count(text):
-    words =  text.split()
-    return len(words)
+class Book:
+    def __init__(self, book_path) -> None:
+        self.__book_path = book_path
+        self.__text = None
 
-def get_chars_count(text):
-    s = {}
-    for char in text.lower():
-        if char in s:
-            s[char] += 1
+    def __get_text(self):
+        if not self.__text:
+            with open(self.__book_path) as f:
+                return f.read()
         else:
-            s[char] = 1
-    return s
+            return self.__text
+
+    def get_text(self):
+        return self.__get_text()
+        
+    def get_book_path(self):
+        return self.__book_path
+        
+    def get_word_count(self):
+        words =  self.__get_text().split()
+        return len(words)
+
+    def get_chars_count(self):
+        chars_dict = {}
+        for char in self.__get_text().lower():
+            if char in chars_dict:
+                chars_dict[char] += 1
+            else:
+                chars_dict[char] = 1
+        return chars_dict
     
 def main():
     book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    print(f"{book_path} contains {len(book_text)} characters")
-    print(f"{book_path} contains {get_word_count(book_text)} words")
-    chars_dict = get_chars_count(book_text)
-    print(f"{book_path} contains {len(get_chars_count(book_text))} unique characters")
+    book = Book(book_path)
+    print(f"{book.get_book_path()} contains {len(book.get_text())} characters")
+    print(f"{book.get_book_path()} contains {book.get_word_count()} words")
+    chars_dict = book.get_chars_count()
+    print(f"{book.get_book_path()} contains {len(book.get_chars_count())} unique characters")
     print(chars_dict)
         
 main()
